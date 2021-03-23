ac45a714daad75e8e7977d0ab426347f34dd5d9a
Oleg Esipov <oleg.esipov@gmail.com>
2021-03-23 21:45:36 +0500

    Add Report

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "340695889111908";
+        String number = "30180972185858";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
